6d1eb17c74c762f2b70cef283cbb5c877037eb63
Doug Richardson <54010293+dougrichardson@users.noreply.github.com>
2025-10-27 20:28:07 +1100

    Minor changes

--- functions.py
+++ functions.py
@@ -15,3 +15,23 @@ def windspeed(u, v):
     v: array of meridional wind
     """
     return np.sqrt(u ** 2 + v ** 2)
+
+def sel_month(ds, month):
+    """
+    Return array for specified month
+    
+    ds: dataset to select from
+    month: int or list of int between 1 and 12, default is None
+    """
+    if month is None:
+        return ds
+    elif isinstance(month, int):
+        if 1 <= month <= 12:
+            return ds.isel(time=ds.time.dt.month == month)
+        else:
+            raise ValueError("Incorrect month specified.")
+    elif isinstance(month, list):
+        if all((isinstance(i, int)) & (1 <= i <= 12) for i in month):
+            return ds.isel(time=ds.time.dt.month.isin(month))
+        else:
+            raise ValueError("Incorrect month specified.")
